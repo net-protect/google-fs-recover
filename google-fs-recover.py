655a643804f3312cfeb99cdcd551d094c7e9e962
McKay Hardy <mckay.hardy@net-protect.io>
2022-02-18 09:33:01 -0700

    Adding protobuf decode. Need to add for duplicate filename checking on copy.

--- google-fs-recover.py
+++ google-fs-recover.py
@@ -5,6 +5,7 @@ import argparse
 import csv
 import shutil
 import hashlib
+import blackboxprotobuf
 
 def GetArgs(_args):#Get cmdline opts
 	allArgs = argparse.ArgumentParser()
@@ -19,34 +20,51 @@ def GetArgs(_args):#Get cmdline opts
 def GetSqlData(_path):
 	#TODO - Need Error Handling. 
 	#Variables
-	selectStr = "SELECT stable_id, local_title from items where is_folder = 0"
-	files = {}
+	#selectStr = "SELECT stable_id, local_title from items where is_folder = 0"
+	selectStr = "SELECT item_stable_id, value from item_properties where key = 'content-entry'"
+	files = []
 	if not (os.path.exists(_path)): #Check if sqllite db exists
 		raise Exception(f"SQLlite database does not exist at {_path}")
 	#sqllite logic
 	conn = sqlite3.connect(_path)
 	cur = conn.cursor()
 	for data in cur.execute(selectStr):
-		files[data[0]] = data[1]
+		files.append({"stableId": data[0], "protobufValue": data[1]})
 	print(f"{len(files)} files found")
 	return files
 
-def GetCahceFiles(_path, _sqlFileData):
+def GetOrigFileNames(_path, _sqlFileData):
+	#TODO - Need Error Handling.
+	if not (os.path.exists(_path)): #Check if sqllite db exists
+			raise Exception(f"SQLlite database does not exist at {_path}")
+	conn = sqlite3.connect(_path)
+	cur = conn.cursor()	
+	#Variables
+	files = []
+	for entry in _sqlFileData:
+		selectStr = f"SELECT stable_id, local_title from items where id = '{entry['itemsId']}'"
+		for data in cur.execute(selectStr):
+			entry["origFileName"] = data[1]
+	return _sqlFileData
+
+def GetCacheFiles(_path, _sqlFileData):
 	#TODO - Need Error Handling.
 	#Variables
 	cacheFileData = []
+	count = 0
 	if not (os.path.exists(_path)): #Check if sqllite db exists
 		raise Exception(f"Google FS Cahce Folder does not exist at {_path}")
 	for root, dirs, file in os.walk(_path):
 		for realFile in file:
 			if not "." in realFile: #Needs to be improved upon
 				#stable_id is offset by +1 from the cached file name. Example cached file 945 is stable_id 946
-				sanitizedFile = int(realFile)
-				sanitizedFile += 1
-				if sanitizedFile  in _sqlFileData.keys():
-					cacheFilePath = os.path.join(root,realFile)
-					md5 = GetMD5Hash(cacheFilePath)
-					cacheFileData.append({"stableId": sanitizedFile,"cacheFileName": realFile, "origFileName": _sqlFileData[sanitizedFile], "cacheFilePath": cacheFilePath, "cacheDirectory": root, "md5Hash": md5})
+				for sqlEntry in _sqlFileData:
+					if int(realFile) == int(sqlEntry["cacheFilename"]):
+						count += 1
+						cacheFilePath = os.path.join(root,realFile)
+						md5 = GetMD5Hash(cacheFilePath)
+						cacheFileData.append({"stableId": sqlEntry["stableId"],"cacheFilename": sqlEntry["cacheFilename"], "origFilename": sqlEntry["origFileName"], "cacheFilePath": cacheFilePath, "cacheDirectory": root, "md5Hash": md5})
+	print (count)
 	return cacheFileData
 
 def WriteCSV(_path, _cacheFileData):
@@ -60,8 +78,7 @@ def WriteCSV(_path, _cacheFileData):
 		writer = csv.writer(f)
 		writer.writerow(headers)
 		for entry in _cacheFileData:
-			
-			data = [entry['stableId'], entry['cacheFileName'], entry['origFileName'], entry['cacheFilePath'], entry['cacheDirectory'], entry['md5Hash']]
+			data = [entry['stableId'], entry['cacheFilename'], entry['origFilename'], entry['cacheFilePath'], entry['cacheDirectory'], entry['md5Hash']]
 			writer.writerow(data)
 		
 		
@@ -69,9 +86,11 @@ def WriteCSV(_path, _cacheFileData):
 def BackupFiles(_path, _cacheFileData):		
 	if not (os.path.exists(_path)): #Check if sqllite db exists
 		os.mkdir(_path)
-	
+	print(_cacheFileData)
 	for cacheFile in _cacheFileData:
-		shutil.copyfile(cacheFile['cacheFilePath'], os.path.join(_path, cacheFile['origFileName']))
+		if not os.path.exists(cacheFile['cacheFilePath']):
+			print(f"File Not Found: {cacheFile['cacheFilePath']}")
+		shutil.copyfile(cacheFile['cacheFilePath'], os.path.join(_path, cacheFile['origFilename']))
 
 def GetMD5Hash(_file):
 	md5Hash = hashlib.md5()
@@ -81,6 +100,31 @@ def GetMD5Hash(_file):
 			md5Hash.update(byte_block)
 		fileHash = md5Hash.hexdigest()
 		return fileHash
+
+def DecodeProtoBuf(_sqlFileData):
+	parsedData = []
+	for file in _sqlFileData:
+		missingKeys = False
+		#print(missingKeys)
+		message,typedefs = blackboxprotobuf.decode_message(file["protobufValue"])
+		if "1" in message.keys():
+			file["cacheFilename"] = message["1"]
+		else:
+			missingKeys = True
+		if "3" in message.keys():
+			file["itemsId"] = message["3"].decode('utf-8')
+		else:
+			missingKeys = True
+		if "4" in message.keys():
+			file["fileSize"] = message["4"]
+		else:
+			missingKeys = True
+		
+		if missingKeys == False:
+			parsedData.append(file)
+		
+	return parsedData
+		
 	
 def main(_args):
 	#Arguments
@@ -92,8 +136,15 @@ def main(_args):
 	#Get SQL File Names
 	sqlFileData = GetSqlData(sqlDb) 
 	
+	#Decode Proto buffer
+	sqlDecodedData = DecodeProtoBuf(sqlFileData)
+	
+	#GetOrigFileNames
+	sqlDecodedDataFiles = GetOrigFileNames(sqlDb, sqlDecodedData)
+	#print(sqlDecodedDataFiles)
+	
 	#Get Cache Files
-	cacheFileData = GetCahceFiles(cacheFiles, sqlFileData)
+	cacheFileData = GetCacheFiles(cacheFiles, sqlDecodedDataFiles)
 	
 	#Write Log file with cachefile names and original file names.
 	WriteCSV(csvDest,cacheFileData)
