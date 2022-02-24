0a550676a6d9d37da19ce6570849e72c87d26e51
McKay Hardy <mckay.hardy@net-protect.io>
2022-02-24 14:40:11 -0700

    New version based on item_properties table and their id

--- google-fs-recover.py
+++ google-fs-recover.py
@@ -6,21 +6,21 @@ import csv
 import shutil
 import hashlib
 import blackboxprotobuf
+from datetime import datetime
 
 def GetArgs(_args):#Get cmdline opts
 	allArgs = argparse.ArgumentParser()
 	
-	allArgs.add_argument("-db","--db", required=True, help="Source of GoogleFS metadata_sqlite_db")
-	allArgs.add_argument("-c","--cache", required=True, help="Source of GoogleFS Cached Files")
-	allArgs.add_argument("-csv","--csv", required=True, help="Destination of CSV output")
+	allArgs.add_argument("-p","--path", required=True, help="Path of GoogleFS")
+	allArgs.add_argument("-csv","--csv", required=False, help="Destination of CSV output")
 	allArgs.add_argument("-b","--backup", required=False, help="Copy files to new location with new file names. Output path.")
+	allArgs.add_argument("-i","--interactive", required=False, help="Interactive mode")
 	
 	return vars(allArgs.parse_args())
 
 def GetSqlData(_path):
 	#TODO - Need Error Handling. 
 	#Variables
-	#selectStr = "SELECT stable_id, local_title from items where is_folder = 0"
 	selectStr = "SELECT item_stable_id, value from item_properties where key = 'content-entry'"
 	files = []
 	if not (os.path.exists(_path)): #Check if sqllite db exists
@@ -30,7 +30,6 @@ def GetSqlData(_path):
 	cur = conn.cursor()
 	for data in cur.execute(selectStr):
 		files.append({"stableId": data[0], "protobufValue": data[1]})
-	print(f"{len(files)} files found")
 	return files
 
 def GetOrigFileNames(_path, _sqlFileData):
@@ -39,12 +38,15 @@ def GetOrigFileNames(_path, _sqlFileData):
 			raise Exception(f"SQLlite database does not exist at {_path}")
 	conn = sqlite3.connect(_path)
 	cur = conn.cursor()	
+	count = 0
 	#Variables
 	files = []
 	for entry in _sqlFileData:
 		selectStr = f"SELECT stable_id, local_title from items where id = '{entry['itemsId']}'"
 		for data in cur.execute(selectStr):
 			entry["origFileName"] = data[1]
+			count += 1
+	print(f"{count} files found in SQLite DB")
 	return _sqlFileData
 
 def GetCacheFiles(_path, _sqlFileData):
@@ -56,23 +58,23 @@ def GetCacheFiles(_path, _sqlFileData):
 		raise Exception(f"Google FS Cahce Folder does not exist at {_path}")
 	for root, dirs, file in os.walk(_path):
 		for realFile in file:
-			if not "." in realFile: #Needs to be improved upon
-				#stable_id is offset by +1 from the cached file name. Example cached file 945 is stable_id 946
-				for sqlEntry in _sqlFileData:
-					if int(realFile) == int(sqlEntry["cacheFilename"]):
-						count += 1
-						cacheFilePath = os.path.join(root,realFile)
-						md5 = GetMD5Hash(cacheFilePath)
-						cacheFileData.append({"stableId": sqlEntry["stableId"],"cacheFilename": sqlEntry["cacheFilename"], "origFilename": sqlEntry["origFileName"], "cacheFilePath": cacheFilePath, "cacheDirectory": root, "md5Hash": md5})
-	print (count)
+			for sqlEntry in _sqlFileData:
+				if str(realFile) == str(sqlEntry["cacheFilename"]):
+					count += 1
+					cacheFilePath = os.path.join(root,realFile)
+					md5 = GetMD5Hash(cacheFilePath)
+					cacheFileData.append({"stableId": sqlEntry["stableId"],"cacheFilename": sqlEntry["cacheFilename"], "origFilename": sqlEntry["origFileName"], "cacheFilePath": cacheFilePath, "cacheDirectory": root, "md5Hash": md5})
+	print(f"{len(cacheFileData)} files found in cache folder")					
 	return cacheFileData
 
 def WriteCSV(_path, _cacheFileData):
 	if not (os.path.exists(_path)): #Check if sqllite db exists
 		raise Exception(f"Output path does not exist {_path}")
-	
-	headers = ['stable_id','cache_filename','original_filename','cache_file_path', 'cache_directory', 'md5']
-	exportFileName = 'google-fs-filelist.csv'
+	count = 0
+	headers = ['stable_id','cache_filename','original_filename','cache_file_path', 'cache_directory', 'md5'] #write the headers for the csv
+	timestamp = datetime.now()
+	formatedTimeStamp = timestamp.strftime("%Y%m%d%H%M%S_%f")
+	exportFileName = f'google-fs-filelist-{formatedTimeStamp}.csv'
 	with open(f"{os.path.join(_path,exportFileName)}", 'w', encoding='UTF8', newline='') as f:
 		allData = []
 		writer = csv.writer(f)
@@ -80,21 +82,24 @@ def WriteCSV(_path, _cacheFileData):
 		for entry in _cacheFileData:
 			data = [entry['stableId'], entry['cacheFilename'], entry['origFilename'], entry['cacheFilePath'], entry['cacheDirectory'], entry['md5Hash']]
 			writer.writerow(data)
-		
-		
+			count += 1
+	print(f"{count} rows written to csv")
+			
 
 def BackupFiles(_path, _cacheFileData):		
 	if not (os.path.exists(_path)): #Check if sqllite db exists
 		os.mkdir(_path)
-	print(_cacheFileData)
+	count = 0
 	for cacheFile in _cacheFileData:
 		if not os.path.exists(cacheFile['cacheFilePath']):
 			print(f"File Not Found: {cacheFile['cacheFilePath']}")
 		else:
-			detinationFile = f"{os.path.join(_path, cacheFile['origFilename'])}" 
+			detinationFile = f"{os.path.join(_path, cacheFile['origFilename'])}"
 			if os.path.exists(detinationFile):
-				detinationFile = f"{detinationFile}_{cacheFile['cacheFilename']}" #Quick and dirty for proof of concept 
+				detinationFile = f"{detinationFile}_{cacheFile['cacheFilename']}"
 			shutil.copyfile(cacheFile['cacheFilePath'], detinationFile)
+			count += 1
+	print(f"{count} files copied to backup folder")
 
 def GetMD5Hash(_file):
 	md5Hash = hashlib.md5()
@@ -109,7 +114,6 @@ def DecodeProtoBuf(_sqlFileData):
 	parsedData = []
 	for file in _sqlFileData:
 		missingKeys = False
-		#print(missingKeys)
 		message,typedefs = blackboxprotobuf.decode_message(file["protobufValue"])
 		if "1" in message.keys():
 			file["cacheFilename"] = message["1"]
@@ -133,8 +137,9 @@ def DecodeProtoBuf(_sqlFileData):
 def main(_args):
 	#Arguments
 	args = GetArgs(_args)
-	sqlDb = args['db']
-	cacheFiles = args["cache"]
+	googlePath = args["path"] #Get path of google fs
+	sqlDb = os.path.join(googlePath, "metadata_sqlite_db")
+	cacheFiles = os.path.join(googlePath, "content_cache")
 	csvDest = args["csv"]
 	backupPath = args["backup"]
 	#Get SQL File Names
@@ -145,13 +150,13 @@ def main(_args):
 	
 	#GetOrigFileNames
 	sqlDecodedDataFiles = GetOrigFileNames(sqlDb, sqlDecodedData)
-	#print(sqlDecodedDataFiles)
 	
 	#Get Cache Files
 	cacheFileData = GetCacheFiles(cacheFiles, sqlDecodedDataFiles)
 	
 	#Write Log file with cachefile names and original file names.
-	WriteCSV(csvDest,cacheFileData)
+	if (csvDest != None):
+		WriteCSV(csvDest,cacheFileData)
 	
 	#Copy Data with Original File Names
 	if (backupPath != None):
